8140e22cc319e9a9b0b980cf63ffc4e46645423d
OlegSadJktu <plyuha2004@mail.ru>
2024-10-18 17:15:29 +0600

    [Feat] Add jsonify

---
+++ pkg/jsonify/jsonify.go
@@ -0,0 +1,19 @@
+package jsonify
+
+import (
+	jsoniter "github.com/json-iterator/go"
+)
+
+func Jsonify(obj interface{}) string {
+	var json = jsoniter.ConfigCompatibleWithStandardLibrary
+	if obj == nil {
+		return ""
+	}
+
+	str, err := json.MarshalIndent(obj, "", " ")
+	if err != nil {
+		return ""
+	}
+
+	return string(str)
+}
